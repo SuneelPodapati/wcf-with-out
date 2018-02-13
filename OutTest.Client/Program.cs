1d8ad7f74cee140d76bf88dbcdefb3a492658dde
SuneelPodapati <podapati.suneel@gmail.com>
2018-02-13 17:25:25 +0530

    Changed the client params

---
+++ OutTest.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using OutTest.Client.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutTest.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new Service.Service1Client();
+            string s1, s2, s3;
+            var response = client.GetData(out s1, out s2, out s3);
+            Console.WriteLine($"{response} - {s1} - {s2} - {s3}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OutTest.WCF/IService1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace OutTest.WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(out string s1, out string s2, out string s3, out string s4);
+        
+
+        // TODO: Add your service operations here
+    }
+}
